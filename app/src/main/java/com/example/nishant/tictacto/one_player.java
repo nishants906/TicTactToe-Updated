ac9e1d50c189ac550d04c765837ee00c529402a3
Nishant Sardana <sardana.nishant@gmail.com>
2017-06-20 20:05:56 +0530

    one player suport added

---
+++ app/src/main/java/com/example/nishant/tictacto/one_player.java
@@ -0,0 +1,13 @@
+package com.example.nishant.tictacto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class one_player extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_player);
+    }
+}
