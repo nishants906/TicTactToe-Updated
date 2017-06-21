2b13de38734d378661da43c9ff9c19eff3a60ed3
Nishant Sardana <sardana.nishant@gmail.com>
2017-06-21 08:06:20 +0530

    one player support added

--- app/src/main/java/com/example/nishant/tictacto/one_player.java
+++ app/src/main/java/com/example/nishant/tictacto/one_player.java
@@ -1,13 +1,38 @@
 package com.example.nishant.tictacto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class one_player extends AppCompatActivity {
+
+    Button easy, hard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_player);
+        easy = (Button) findViewById(R.id.easy);
+        easy.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(one_player.this, one_p_easy.class);
+                        startActivity(intent);
+                    }
+                }
+        );
+        hard = (Button) findViewById(R.id.hard);
+        hard.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(one_player.this, one_p_difficult.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
